828c62c0ec49c5f14f30e6b528ab4c1fd637b78c
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2025-09-11 10:44:11 +0900

    update

--- 250911/wise.py
+++ 250911/wise.py
@@ -2,12 +2,35 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import time
+from datetime import datetime
 
 
 url = "https://comp.wisereport.co.kr/company/c1010001.aspx?cmp_cd="
 
 # 종목코드 
-codes = ['000660', '005930', '053280']
+# codes = ['000660', '005930', '053280']
+codes = []
+# 유저가 입력한 값을 codes에 추가 
+# 최대codes의 개수는 10개 
+# 유저가 입력한 값이 존재하지 않으면 추가작업도 종료
+for i in range(10):
+    # 유저가 값을 입력한다. 
+    input_code = input("검색하려는 종목의 코드를 입력하시오 : ")
+    # input_code가 존재하지 않는다면 반복문을 종료
+    if input_code:
+        codes.append(input_code)
+    else:
+        break
+
+# while True:
+#     input_code = input("검색하려는 종목의 코드를 입력하시오 : ")
+#     if input_code:
+#         codes.append(input_code)
+#         if len(codes) == 10:
+#             break
+#     else:
+#         break
+
 
 # 빈 데이터프레임을 생성 
 df = pd.DataFrame()
@@ -51,5 +74,9 @@ for code in codes:
     time.sleep(1)
     # 반복 실행될때마다 로그를 추가 진행 상황 확인 
     print(f"{code} 데이터 수집 완료")
+
+# 현재시간을 불러온다. 
+now_time = datetime.now() # 현재의 시간을 로드 -> 나노초 표시
+now_str = now_time.strftime('%Y%m%d_%H%M%S')   # ->현재 시간의 포멧을 년월일_시분초
 # df를 csv 파일로 저장 
-df.to_csv('wise_data.csv', index=False)
+df.to_csv(f'wise_data{now_str}.csv', index=False)
