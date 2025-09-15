cc35ca539c76278bc63715d19e9d20e8c13c9e30
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2025-09-15 17:49:44 +0900

    update

--- 250915/app.py
+++ 250915/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, url_for
-
+import pandas as pd
 # __name__ : 현재 파일의 이름
 app = Flask(__name__)
 
@@ -14,15 +14,58 @@ app = Flask(__name__)
 def index():
     # 현재 작업 경로에서 하위 디렉토리인 
     # templates 안에 있는 index.html를 되돌려준다.
-    return render_template('index.html')
+    # 샘플 데이터프레임을 로드 
+    # 현재 경로에서 static 폴더로 이동 data 폴더로 이동해서 AAPL.csv
+    df = pd.read_csv('static/data/AAPL.csv')
+    # 하위의 데이터 30개만 필터
+    df = df.tail(30)
+    # index.html에서 table에 컬럼의 이름들을 사용하기 위해 list형태의 
+    # columns를 생성 
+    cols = list(df.columns)
+    values = df.to_dict(orient='records')
+    # table에 데이터의 값들을 [{}, {}, {}] 형태로 생성 
+    # chart에서 사용할 x축의 데이터와 y축의 데이터를 list형태로 생성 
+    x = df['Date'].to_list()
+    y = df['Volume'].to_list()
+    # cols와 values를 이용하여 index.html에서 table태그를 생성
+    return render_template('index.html', 
+                           cols = cols, 
+                           values = values, 
+                           x = x, 
+                           y = y)
 
 @app.route('/second')
 def second():
-    return render_template('index2.html')
+    # AAPL 로드 
+    df = pd.read_csv("static/data/AAPL.csv")
+    df = df.tail(30)
+    cols = list(df.columns)
+    values = df.to_dict(orient='records')
+    x = df['Date'].to_list()
+    y = df['Adj Close'].to_list()
+    return render_template('index2.html', 
+                           cols = cols, 
+                           values = values,
+                           x = x, 
+                           y = y)
 
 @app.route('/third')
 def third():
-    return render_template('index3.html')
+    # AAPL 데이터에서 y축의 데이터를 2개를 사용 
+    # 저가와 고가를 사용
+    df = pd.read_csv('static/data/AAPL.csv')
+    df = df.tail(30)
+    cols = list(df.columns)
+    values = df.to_dict(orient='records')
+    x = df['Date'].to_list()
+    y = df['Low'].to_list()
+    y2 = df['High'].to_list()
+    return render_template('index3.html', 
+                           columns = cols, 
+                           value_data = values, 
+                           date = x, 
+                           low_data = y, 
+                           high_data = y2)
 
 
 
--- 250915/static/js/dashboard.js
+++ 250915/static/js/dashboard.js
@@ -4,35 +4,27 @@
   'use strict'
 
   // Graphs
+  // 문서 안에 있는 ID가 myChart인 태그를 선택하여 ctx 변수에 저장
   const ctx = document.getElementById('myChart')
+  // ctx에 그래프를 그려준다.
   new Chart(ctx, {
-    type: 'line',
+    // 어떤 그래프를 그릴것인가?
+    type: 'bar',
     data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
+      // x축의 데이터를 대입 
+      labels: {{x | tojson}},
+      // y축의 데이터를 대입( 복수 y 축 데이터 가능 )
+      datasets: [
+        {
+          // 실제 y축 데이터가 대입되는 부분
+          data: {{y | tojson}},
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }
+      ]
     },
     options: {
       plugins: {
